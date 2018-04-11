65bf69306d459c5e3d16a874d204e07376c6dd86
Prince Kumar <neo11prince@gmail.com>
2018-04-12 00:14:24 +0530

    UserInput

--- demandPaged.c
+++ demandPaged.c
@@ -1,18 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 double page_fault_rate();
+void userInput(void);
+
+double service_page_fault_empty;
+double service_page_fault_modified;
+double mem_access_time;
+double times_page_modified;
+double effective_access_time;
+double pageFaultRate;
+double service_page_fault_empty_ns;
+double service_page_fault_modified_ns;
+double times_page_modified_per;
+	
 
 void main(){
-	double service_page_fault_empty;
-	double service_page_fault_modified;
-	double mem_access_time;
-	double times_page_modified;
-	double effective_access_time;
-	double pageFaultRate;
-	double service_page_fault_empty_ns;
-	double service_page_fault_modified_ns;
-	double times_page_modified_per;
+	int swtch;
 	
-	while(1){
+	do{
+	
+	
+	printf("\n Select the required option \n");
+	printf("1.Find the PageFault Rate\n");
+	printf("2.Exit");
+	scanf("%d",&swtch);
+	switch(swtch){
+		case 1:userInput();break;
+		case 2:exit(0);
+	}
+
+}while(swtch<3);
+}
+void userInput(){
+	
+	
 	printf("\nEnter service Page Fault [Empty|Page is not Modified]");
 	scanf("%lf",&service_page_fault_empty);
 	printf("Enter Service Page Fault [Modified Page]");
@@ -29,8 +50,10 @@ void main(){
 	times_page_modified_per = (times_page_modified/100);   
     pageFaultRate =  page_fault_rate(service_page_fault_empty_ns,service_page_fault_modified_ns,mem_access_time,times_page_modified_per,effective_access_time);
 	printf("Page Fault rate = %e",pageFaultRate);
+
+
 }
-}
+
 double page_fault_rate(double servicePageFaultEmpty,double servicePageFaultMod,double memAccess,double timesPages,double effAccess){
 	double assume,serve;
 	double numErator,denOminator;
